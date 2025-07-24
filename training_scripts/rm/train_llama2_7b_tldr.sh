15683eb7ba22169872fbad41a47b88f79348ac34
JiancongXiao <42569308+JiancongXiao@users.noreply.github.com>
2025-07-24 02:15:18 -0400

    Add files via upload

---
+++ training_scripts/rm/train_llama2_7b_tldr.sh
@@ -0,0 +1,46 @@
+
+
+set -e 
+set -x
+
+export HF_DATASETS_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1
+
+# DeepSpeed Team
+BASE_PATH="./home"
+DATA_PATH="${BASE_PATH}/datasets/tl-dr"
+MODEL_NAME="meta-llama/Llama-2-7b-hf"
+ZERO_STAGE=2
+
+LOG_PATH="./log"
+SEED=1234
+
+TIME_STEP=`date "+%Y-%m-%d-%H-%M-%S"`
+OUTPUT="${LOG_PATH}/step2_reward-Llama2_7b-tl_dr-$TIME_STEP-$SEED"
+
+mkdir -p $OUTPUT
+
+deepspeed --master_port 12347 --include=localhost:4,5,6,7 step2_reward_model_finetuning/main.py \
+   --data_path $DATA_PATH \
+   --data_output_path "${BASE_PATH}/tmp/data_files/rlhf_consistency/llama2" \
+   --data_split 2,4,4 \
+   --model_name_or_path $MODEL_NAME \
+   --per_device_train_batch_size 36 \
+   --per_device_eval_batch_size 36 \
+   --max_seq_len 512 \
+   --learning_rate 1e-5 \
+   --weight_decay 0.1 \
+   --num_padding_at_beginning 0 \
+   --num_train_epochs 2  \
+   --gradient_accumulation_steps 1 \
+   --lr_scheduler_type cosine \
+   --num_warmup_steps 0 \
+   --seed $SEED \
+   --gradient_checkpointing \
+   --zero_stage $ZERO_STAGE \
+   --deepspeed \
+   --dtype bf16 \
+   --output_dir $OUTPUT \
+   --enable_tensorboard \
+   --print_loss \
+  2>&1 | tee $OUTPUT/training.log
